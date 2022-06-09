c0e0db623758015bc47c1a160f9577a038d219a6
Ievgen Vovk <vovk@icrr.u-tokyo.ac.jp>
2022-06-09 15:05:41 +0900

    Smoothly broken PWL model.

--- srcsim/spec.py
+++ srcsim/spec.py
@@ -17,6 +17,16 @@ def generator(config):
         spec = lambda e: power_law(e, norm=cfg['norm'], e0=cfg['e0'], index=cfg['index'])
     elif cfg['type'] == 'pwlec':
         spec = lambda e: power_law_ecut(e, norm=cfg['norm'], e0=cfg['e0'], index=cfg['index'], ecut=cfg['ecut'])
+    elif cfg['type'] == 'sbpwl':
+        spec = lambda e: smoothly_broken_power_law(
+            e,
+            norm=cfg['norm'],
+            e0=cfg['e0'],
+            ebr=cfg['ebr'],
+            index=cfg['index'],
+            index_delta=cfg['index_delta'],
+            smoothing=cfg['smoothing']
+        )
     elif cfg['type'] == 'lp':
         spec = lambda e: logparabola(e, norm=cfg['norm'], e0=cfg['e0'], index=cfg['index'], beta=cfg['beta'])
     else:
@@ -31,6 +41,13 @@ def power_law(e, e0, norm, index):
 
 def power_law_ecut(e, e0, norm, index, ecut):
     return norm * (e/e0).decompose()**index * np.exp(-(e/ecut))
+
+
+def smoothly_broken_power_law(e, e0, norm, index, index_delta, ebr, smoothing):
+    """
+    From Eq.9 in https://ui.adsabs.harvard.edu/abs/2021PhRvL.126t1102A/abstract
+    """
+    return norm * (e/e0).decompose()**index * (1 + (e/ebr).decompose()**smoothing)**(index_delta / smoothing)
 
 
 def logparabola(e, e0, norm, index, beta):
