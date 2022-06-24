c424873dd5457e33766e4e7e02aef2eee023e36c
Ievgen Vovk <vovk@icrr.u-tokyo.ac.jp>
2022-06-24 12:13:05 +0900

    AltAzBoxGenerator: wobble support.

--- srcsim/rungen.py
+++ srcsim/rungen.py
@@ -80,6 +80,15 @@ class AltAzBoxGenerator:
             frame='icrs'
         )
 
+        if cfg['pointing']['wobble'] is not None:
+            wobble_offset = u.Quantity(cfg['pointing']['wobble']['offset'])
+            wobble_start_angle = u.Quantity(cfg['pointing']['wobble']['start_angle'])
+            wobble_count = cfg['pointing']['wobble']['count']
+        else:
+            wobble_offset = None
+            wobble_start_angle = None
+            wobble_count = None
+
         azmin = u.Quantity(cfg['box']['az']['min'])
         azmax = u.Quantity(cfg['box']['az']['max'])
         altmin = u.Quantity(cfg['box']['alt']['min'])
@@ -95,10 +104,17 @@ class AltAzBoxGenerator:
             height=u.Quantity(cfg['location']['height']),
         )
 
-        return cls.get_runs(obsloc, tel_pos, duration, altmin, altmax, azmin, azmax, tstart, accuracy, max_run_duration)
+        return cls.get_runs(obsloc, tel_pos, duration, altmin, altmax, azmin, azmax, tstart, accuracy, max_run_duration, wobble_offset, wobble_start_angle, wobble_count)
 
     @classmethod
-    def get_runs(cls, obsloc, tel_pos, tobs, altmin, altmax, azmin, azmax, tstart=None, accuracy=1*u.minute, max_run_duration=None):
+    def get_runs(cls, obsloc, tel_pos, tobs, altmin, altmax, azmin, azmax, tstart=None, accuracy=1*u.minute, max_run_duration=None, wobble_offset=None, wobble_start_angle=None, wobble_count=None):
+        wobble_params = (wobble_offset, wobble_start_angle, wobble_count)
+        n_params_set = sum([par is not None for par in wobble_params])
+        use_wobble = n_params_set == len(wobble_params)
+
+        if not use_wobble and n_params_set > 0:
+            raise ValueError(f"must specify all or none of the ['wobble_offset', 'wobble_start_angle', 'wobble_count'] parameters")
+
         if tstart is None:
             tstart = Time('1970-01-01')
 
@@ -164,9 +180,24 @@ class AltAzBoxGenerator:
             tstarts, tstops = get_time_intervals(tel_altaz, altmin, altmax, azmin, azmax, accuracy, max_run_duration)
             tstops = Time(tstarts + tobs / (len(tstarts)))
 
-        runs = tuple(
-            DataRun(tel_pos, tstart, tstop, obsloc, run_id)
-            for run_id, (tstart, tstop) in enumerate(zip(tstarts, tstops))
-        )
+        if use_wobble:
+            pos_angles = wobble_start_angle + np.linspace(0, 2* np.pi, num=wobble_count+1)[:-1] * u.rad
+
+            runs = tuple(
+                DataRun(
+                    tel_pos.directional_offset_by(pos_angles[run_id % wobble_count], wobble_offset),
+                    tstart,
+                    tstop,
+                    obsloc,
+                    run_id
+                )
+                for run_id, (tstart, tstop) in enumerate(zip(tstarts, tstops))
+            )
+
+        else:
+            runs = tuple(
+                DataRun(tel_pos, tstart, tstop, obsloc, run_id)
+                for run_id, (tstart, tstop) in enumerate(zip(tstarts, tstops))
+            )
         
         return runs
