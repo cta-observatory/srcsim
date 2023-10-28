730f07996262d0bc48b9893ab4d271f66da8978d
Ievgen Vovk <vovk@icrr.u-tokyo.ac.jp>
2023-10-28 17:28:21 +0900

    FixedObsGenerator class prototype

---
+++ src/srcsim/rungen/fixed.py
@@ -0,0 +1,167 @@
+import yaml
+import numpy as np
+import pandas as pd
+import astropy.units as u
+from scipy.interpolate import CubicSpline
+from astropy.time import Time
+from astropy.coordinates import SkyCoord, SkyOffsetFrame, EarthLocation, AltAz
+
+from srcsim.rungen import get_trajectory, get_time_intervals
+from srcsim.run import FixedPointingDataRun
+
+
+class FixedObsGenerator:
+    @classmethod
+    def get_runs_from_config(cls, config):
+        if isinstance(config, str):
+            cfg = yaml.load(open(config, "r"), Loader=yaml.FullLoader)
+        else:
+            cfg = config
+
+        tstart = Time(cfg['time']['start'])
+        ra_width = u.Quantity(cfg['pointing']['width'])
+        alt = u.Quantity(cfg['pointing']['center']['alt'])
+        tel_pos = SkyCoord(
+            u.Quantity(cfg['pointing']['center']['ra']),
+            u.Quantity(cfg['pointing']['center']['dec']),
+            frame='icrs'
+        )
+
+        duration = u.Quantity(cfg['time']['duration'])
+
+        obsloc = EarthLocation(
+            lat=u.Quantity(cfg['location']['lat']),
+            lon=u.Quantity(cfg['location']['lon']),
+            height=u.Quantity(cfg['location']['height']),
+        )
+
+        return cls.get_runs(obsloc, tel_pos, duration, alt, ra_width, tstart)
+
+    @classmethod
+    def get_runs(cls, obsloc, tel_pos, tobs, alt, ra_width, tstart=None):
+        time_step = 1 * u.m
+
+        tel_pos_trail = SkyCoord(
+            tel_pos.icrs.ra + ra_width / 2,
+            tel_pos.icrs.dec,
+            frame='icrs'
+        )
+        tel_pos_lead = SkyCoord(
+            tel_pos.icrs.ra - ra_width / 2,
+            tel_pos.icrs.dec,
+            frame='icrs'
+        )
+
+        # First pass - first day
+        track_trail = get_trajectory(
+            tel_pos_trail.icrs,
+            tstart,
+            tstop=tstart + 1 * u.d,
+            time_step=time_step,
+            obsloc=obsloc
+        )
+        track_lead = get_trajectory(
+            tel_pos_lead.icrs,
+            tstart,
+            tstop=tstart + 1 * u.d,
+            time_step=time_step,
+            obsloc=obsloc
+        )
+        cs_lead = CubicSpline(
+            track_lead.obstime.mjd,
+            track_lead.alt - alt
+        )
+        cs_trail = CubicSpline(
+            track_trail.obstime.mjd,
+            track_trail.alt - alt
+        )
+
+        tstarts = Time(cs_lead.roots(extrapolate=False), format='mjd')
+        tstops = Time(cs_trail.roots(extrapolate=False), format='mjd')
+        
+        # Total number of the required observation days
+        run_durations = tstops - tstarts
+        nsequences = (tobs / np.sum(run_durations)).decompose()
+        ndays_full = np.floor(nsequences)
+        ndays_total = np.ceil(nsequences)
+
+        if ndays_full > 0:
+            # Next pass - to cover the simulation time interval with fully completed runs
+            track_lead = get_trajectory(
+                tel_pos_lead.icrs,
+                tstart,
+                tstop=tstart + ndays_full * u.d,
+                time_step=time_step,
+                obsloc=obsloc
+            )
+            track_trail = get_trajectory(
+                tel_pos_trail.icrs,
+                tstart,
+                tstop=tstart + ndays_full * u.d,
+                time_step=time_step,
+                obsloc=obsloc
+            )
+
+            cs_lead = CubicSpline(
+                track_lead.obstime.mjd,
+                track_lead.alt - alt
+            )
+            cs_trail = CubicSpline(
+                track_trail.obstime.mjd,
+                track_trail.alt - alt
+            )
+            tstarts = Time(cs_lead.roots(extrapolate=False), format='mjd')
+            tstops = Time(cs_trail.roots(extrapolate=False), format='mjd')
+            remaining_tobs = tobs - np.sum(tstops - tstarts)
+            
+            # Next pass - additional incomplete runs
+            track_lead = get_trajectory(
+                tel_pos_lead.icrs,
+                tstart=tstart + ndays_full * u.d,
+                tstop=tstart + ndays_total * u.d,
+                time_step=time_step,
+                obsloc=obsloc
+            )
+            cs_lead = CubicSpline(
+                track_lead.obstime.mjd,
+                track_lead.alt - alt
+            )
+            _tstarts = Time(cs_lead.roots(extrapolate=False), format='mjd')
+            _tstops = Time(_tstarts + remaining_tobs / (len(_tstarts)))
+
+            tstarts = Time([tstarts, _tstarts])
+            tstops = Time([tstops, _tstops])
+
+        else:
+            track_lead = get_trajectory(
+                tel_pos_lead.icrs,
+                tstart,
+                tstop=tstart + ndays_full * u.d,
+                time_step=time_step,
+                obsloc=obsloc
+            )
+            track_trail = get_trajectory(
+                tel_pos_trail.icrs,
+                tstart,
+                tstop=tstart + ndays_full * u.d,
+                time_step=time_step,
+                obsloc=obsloc
+            )
+
+            cs_lead = CubicSpline(
+                track_lead.obstime.mjd,
+                track_lead.alt - alt
+            )
+            cs_trail = CubicSpline(
+                track_trail.obstime.mjd,
+                track_trail.alt - alt
+            )
+            tstarts = Time(cs_lead.roots(extrapolate=False), format='mjd')
+            tstops = Time(tstarts + tobs / (len(tstarts)))
+
+        runs = tuple(
+            FixedPointingDataRun(tel_pos, tstart, tstop, obsloc, run_id)
+            for run_id, (tstart, tstop) in enumerate(zip(tstarts, tstops))
+        )
+
+        return runs
