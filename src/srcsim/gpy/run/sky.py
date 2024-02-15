93a701d3479b57b1e2178b66f4703dab23b3b406
Ievgen Vovk <vovk@icrr.u-tokyo.ac.jp>
2024-02-15 10:48:29 +0900

    SkyDataRun & FixedPointingDataRun: refactoring to inherit common functionality from DataRun.

--- src/srcsim/gpy/run/sky.py
+++ src/srcsim/gpy/run/sky.py
@@ -52,6 +52,14 @@ class SkyDataRun(DataRun):
 
         return data_run
 
+    @property
+    def pointing(self):
+        return FixedPointingInfo(fixed_icrs=self.tel_pos, mode=PointingMode.POINTING)
+
+    @property
+    def tel_pos_center_icrs(self):
+        return self.tel_pos.icrs
+
     def to_dict(self):
         data = {'id': self.id, 'pointing': {}, 'time': {}, 'location': {}}
 
@@ -66,89 +74,6 @@ class SkyDataRun(DataRun):
         data['location']['height'] = str(self.obsloc.height.to('m').to_string())
 
         return data
-    
-    def predict(self, irfs, models, tel_pos_tolerance=None):
-        frame_tref = AltAz(
-            obstime=Time(
-                (self.tstart.mjd + self.tstop.mjd) / 2,
-                format='mjd'
-            ),
-            location=self.obsloc
-        )
 
-        tel_pos = self.tel_pos.transform_to(frame_tref)
-        
-        if tel_pos_tolerance is None:
-            irfs = irfs.get_closest(tel_pos.altaz)
-        elif isinstance(tel_pos_tolerance, u.Quantity):
-            irfs = irfs.get_nearby(tel_pos, tel_pos_tolerance)
-        elif isinstance(tel_pos_tolerance, list) or isinstance(tel_pos_tolerance, tuple):
-            irfs = irfs.get_in_box(
-                tel_pos,
-                max_lon_offset=tel_pos_tolerance[0],
-                max_lat_offset=tel_pos_tolerance[1],
-            )
-        else:
-            raise ValueError(f"Data type '{type(tel_pos_tolerance)}' for argument 'tel_pos_tolerance' is not supported")
-
-        pointing = FixedPointingInfo(fixed_icrs=self.tel_pos, mode=PointingMode.POINTING)
-
-        observation = Observation.create(
-            pointing=pointing,
-            location=self.obsloc,
-            obs_id=self.id,
-            tstart=self.tstart,
-            tstop=self.tstop,
-            irfs=irfs.samples[0].to_dict()
-        )
-        observation.aeff.meta["TELESCOP"] = 'cta_north'
-
-        energy_axis = MapAxis.from_energy_bounds(
-            "0.03 TeV",
-            "100 TeV",
-            nbin=20,
-            per_decade=True
-        )
-        energy_axis_true = MapAxis.from_energy_bounds(
-            "0.01 TeV",
-            "300 TeV",
-            nbin=30,
-            per_decade=True,
-            name="energy_true"
-        )
-        migra_axis = MapAxis.from_bounds(
-            0.5,
-            2,
-            nbin=150,
-            node_type="edges",
-            name="migra"
-        )
-
-        geom = WcsGeom.create(
-            skydir=observation.pointing.fixed_icrs,
-            width=(5, 5),
-            binsz=0.1,
-            frame="icrs",
-            axes=[energy_axis],
-        )
-
-        empty = MapDataset.create(
-            geom,
-            energy_axis_true=energy_axis_true,
-            migra_axis=migra_axis,
-            name="my-dataset",
-        )
-
-        maker = MapDatasetMaker(
-            selection=["exposure", "background", "psf", "edisp"]
-        )
-        dataset = maker.run(empty, observation)
-
-        dataset.models = models
-
-        sampler = MapDatasetEventSampler(random_state=0)
-        events = sampler.run(dataset, observation)
-
-        observation._events = events
-
-        return observation
+    def tel_pos_to_altaz(self, frame):
+        return self.tel_pos.transform_to(frame)
